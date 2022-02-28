ed07e01c3235b92a29e77f1d088d380951fe6c3c
Leshan <leshan0627@gmail.com>
2022-02-28 17:23:07 -0500

    fix: Fix studentGrade function by pushing JSX to an array and return an array

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,21 +1,35 @@
 // Imports
 import React from "react";
 import "../Style/Students.css";
+import { FaPlus } from "react-icons/fa";
 
 // Student function for showing data from API
 export default function Student(props) {
   const { filteredStudents, students, inputValue } = props;
 
-  console.log("HEREFIVE",filteredStudents )
+  console.log("HEREFIVE", filteredStudents);
 
-  // Return student grades
-  const studentGrades = function (student) {
+  // Return student average
+  const studentAverage = function (student) {
     let totalgrades = 0;
     for (let grade of student.grades) {
       totalgrades += Number(grade);
     }
     return totalgrades / student.grades.length + "%";
   };
+
+   // Return student agrades
+   const studentGrades = (student) => {
+    let index = [];
+    for (let i = 0; i < student.grades.length; i++) {
+     index.push(
+       <div className="student_grades">
+        <p>Test {i + 1}: {student.grades[i]}%</p>
+       </div>
+     )
+    } return index
+  };
+   
 
   // Map through student object and return data
   const studentList = students.map((student) => {
@@ -25,14 +39,18 @@ export default function Student(props) {
           <img src={student.pic} alt={student.firstName} />
         </section>
         <section>
-          <h1 className="student_name">
-            {student.firstName.toUpperCase()} {student.lastName.toUpperCase()}
-          </h1>
+          <div className="article_header">
+            <h1 className="student_name">
+              {student.firstName.toUpperCase()} {student.lastName.toUpperCase()}
+            </h1>
+            <FaPlus className="icon" />
+          </div>
           <div className="student_info">
             <p>Email: {student.email}</p>
             <p>Company: {student.company}</p>
             <p>Skill: {student.skill}</p>
-            <p>Average: {studentGrades(student)}</p>
+            <p>Average: {studentAverage(student)}</p>
+            {studentGrades(student)}
           </div>
         </section>
       </article>
@@ -47,22 +65,25 @@ export default function Student(props) {
           <img src={filteredStudent.pic} alt={filteredStudent.firstName} />
         </section>
         <section>
-          <h1 className="student_name">
-            {filteredStudent.firstName.toUpperCase()}{" "}
-            {filteredStudent.lastName.toUpperCase()}
-          </h1>
+          <div className="article_header">
+            <h1 className="student_name">
+              {filteredStudent.firstName.toUpperCase()}{" "}
+              {filteredStudent.lastName.toUpperCase()}
+            </h1>
+            <FaPlus className="icon" />
+          </div>
           <div className="student_info">
             <p>Email: {filteredStudent.email}</p>
             <p>Company: {filteredStudent.company}</p>
             <p>Skill: {filteredStudent.skill}</p>
-            <p>Average: {studentGrades(filteredStudent)}</p>
+            <p>Average: {studentAverage(filteredStudent)}</p>
+            {studentGrades(filteredStudent)}
           </div>
         </section>
       </article>
-      );
-    }
-  )
-  
+    );
+  });
+
   return (
     <div>
       {filteredStudents.length !== 0 && filteredStudentObject}
